ce7a2564bc050f359fa6e728e732d5b62bf675dc
Nazrul-dev-91 <nazrulislam41291@gmail.com>
2026-05-14 01:39:52 +0600

    Update views.py

--- nazrulapp/views.py
+++ nazrulapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from tawhidapp.models import *
+from nazrulapp.models import *
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
